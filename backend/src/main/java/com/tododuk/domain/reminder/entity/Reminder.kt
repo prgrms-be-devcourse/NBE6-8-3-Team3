1d43196a719e6f1ab83074fce3b783063196fce0
안홍선 <moon72907@gmail.com>
2025-08-28 12:47:57 +0900

    reminder 엔티티 코틀린으로 마이그레이션

--- backend/src/main/java/com/tododuk/domain/reminder/entity/Reminder.kt
+++ backend/src/main/java/com/tododuk/domain/reminder/entity/Reminder.kt
@@ -1,30 +1,25 @@
-package com.tododuk.domain.reminder.entity;
+package com.tododuk.domain.reminder.entity
 
-import com.tododuk.domain.todo.entity.Todo;
-import com.tododuk.global.entity.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.tododuk.domain.todo.entity.Todo
+import com.tododuk.global.entity.BaseEntity
+import jakarta.persistence.Entity
+import jakarta.persistence.ManyToOne
+import java.time.LocalDateTime
 
-
-import java.time.LocalDateTime;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-public class Reminder extends BaseEntity {
-    @ManyToOne
-    private Todo todo;
-    private LocalDateTime remindAt;
-    private String method; //알림 내용? 알림 방법?
+class Reminder (
+    @ManyToOne()
+    var todo : Todo,
+    var remindAt: LocalDateTime,
+    var method  : String
+) : BaseEntity(){
 
-    public Reminder(Todo todo, LocalDateTime remindAt, String method) {
-        this.todo = todo;
-        this.remindAt = remindAt;
-        this.method = method;
-    }
 
+
+    protected constructor() : this(
+        todo = Todo(),
+        remindAt = LocalDateTime.now(),
+        method = ""
+    )
 }
